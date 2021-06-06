3a17b22d9e8367cc1773d1d7905727277a436865
RColes <rfcoles@hotmail.co.uk>
2021-06-06 19:09:20 -0300

    moved data balanced to after test-train split

--- Utils/datasets.py
+++ Utils/datasets.py
@@ -54,38 +54,6 @@ def load_full(balanced=True):
     try:
         img, z, _, _, _ = pickle.load(open("../Data/data_full.pickle","rb"))
         out_size = 1
-        if balanced:
-            bins = 20
-            bin_amount = int(len(z)/bins)
-            
-            idx = [i for i, z in enumerate(z) if z > 3.5]
-            img = np.delete(img, idx,axis=0)
-            z = np.delete(z, idx)
-            
-            idx = [i for i, z in enumerate(z) if z < 0.5]
-            img = np.delete(img, idx,axis=0)
-            z = np.delete(z, idx)
-    
-            n, bin_edges = np.histogram(z,bins)
-            idxs = np.digitize(z, bin_edges)-1
-            new_imgs = [] 
-            new_z = []
-            for j in range(len(n)):
-                idx = [ix for ix, i in enumerate(idxs) if i==j]
-                if n[j] > bin_amount:
-                    idx = np.random.choice(idx, bin_amount, replace=False)
-                    new_imgs.append(img[idx,:,:,:])
-                    new_z.append(z[idx])
-                else:
-                    idx = np.hstack([idx, np.random.choice(idx,bin_amount-n[j])])
-                    new_imgs.append(img[idx,:,:,:])
-                    new_z.append(z[idx])
-                     
-            img = [item for sublist in new_imgs for item in sublist]
-            img = np.array(img)
-            z = [item for sublist in new_z for item in sublist]
-            z = np.array(z)
-      
         return img, z, np.shape(img[0]), out_size 
     except:
         print("Could not load galaxy data")
@@ -98,45 +66,48 @@ def load_dist():
     except:
         print("Could not load galaxy data")
 
-
-
-class Scaler():
-    def __init__(self, dims):
-        self.dims = dims
-        self.x_min = np.zeros(self.dims)
-        self.x_max = np.zeros(self.dims)
-        self.y_min = 0
-        self.y_max = 0
-
-    def minmax_img(self, x, bychannels=True):
-        if bychannels:
-            for i in range(self.dims):
-                self.x_min[i] = np.min(x[:,:,:,i])
-                self.x_max[i] = np.max(x[:,:,:,i])
-                x[:,:,:,i] = (x[:,:,:,i] - self.x_min[i])/(self.x_max[i] - self.x_min[i])
+def rebalance_dataset(img, z, z_min, z_max, bins=20):
+    bin_amount = int(len(z)/bins)
+    idx = [i for i, z in enumerate(z) if z > z_max]
+    img = np.delete(img, idx,axis=0)
+    z = np.delete(z, idx)
+   
+    idx = [i for i, z in enumerate(z) if z < z_min]
+    img = np.delete(img, idx,axis=0)
+    z = np.delete(z, idx)
+   
+    n, bin_edges = np.histogram(z,bins)
+    idxs = np.digitize(z, bin_edges)-1
+    new_imgs = [] 
+    new_z = []
+    for j in range(len(n)):
+        idx = [ix for ix, i in enumerate(idxs) if i==j]
+        if n[j] > bin_amount:
+            idx = np.random.choice(idx, bin_amount, replace=False)
+            new_imgs.append(img[idx,:,:,:])
+            new_z.append(z[idx])
         else:
-            self.x_min[0] = np.min(x)
-            self.x_max[0] = np.max(x)
-            x = (x-self.x_min[0])/(self.x_max[0]-self.x_min[0])
-        return x
+            idx = np.hstack([idx, np.random.choice(idx,bin_amount-n[j])])
+            new_imgs.append(img[idx,:,:,:])
+            new_z.append(z[idx])
+                    
+    img = [item for sublist in new_imgs for item in sublist]
+    img = np.array(img)
+    z = [item for sublist in new_z for item in sublist]
+    z = np.array(z)
 
-    def minmax_z(self,y):
-        self.y_min = np.min(y)
-        self.y_max = np.max(y)
-        y = (y-self.y_min)/(self.y_max-self.y_min)
-        return y
+    return img, z    
 
 
 class Loader():
     def __init__(self, test_per, dat, balanced=False):
-        
         self.datasets = {
             "load_test": load_test(),
-            "load_full": load_full(balanced),
+            "load_full": load_full(),
             "load_dist": load_dist()}
 
         x, y, self.shape, self.num_out = self.datasets[dat]
-
+        
         self.dims = self.shape[-1]
         self.scaler = Scaler(self.dims)
         #x_scaled = np.arcsinh(x)
@@ -146,7 +117,16 @@ class Loader():
         self.test_per = test_per
         self.x_train, self.x_test, self.y_train, self.y_test = \
             train_test_split(x,y_scaled, test_size=self.test_per, random_state=seed)
-                    
+        
+        if balanced==True:
+            bins = 10
+            z_min = 0.5
+            z_max = 6.5
+            z_min = self.scaler.norm(z_min)
+            z_max = self.scaler.norm(z_max)
+            self.x_train, self.y_train = rebalance_dataset(\
+                                        self.x_train, self.y_train, z_min, z_max, bins)
+       
         self.reset()
 
     def reset(self):
@@ -177,4 +157,33 @@ class Loader():
 
     def get_test(self):
         return self.x_test, self.y_test
-               
+            
+
+class Scaler():
+    def __init__(self, dims):
+        self.dims = dims
+        self.x_min = np.zeros(self.dims)
+        self.x_max = np.zeros(self.dims)
+        self.y_min = 0
+        self.y_max = 0
+
+    def minmax_img(self, x, bychannels=True):
+        if bychannels:
+            for i in range(self.dims):
+                self.x_min[i] = np.min(x[:,:,:,i])
+                self.x_max[i] = np.max(x[:,:,:,i])
+                x[:,:,:,i] = (x[:,:,:,i] - self.x_min[i])/(self.x_max[i] - self.x_min[i])
+        else:
+            self.x_min[0] = np.min(x)
+            self.x_max[0] = np.max(x)
+            x = (x-self.x_min[0])/(self.x_max[0]-self.x_min[0])
+        return x
+
+    def minmax_z(self,y):
+        self.y_min = np.min(y)
+        self.y_max = np.max(y)
+        y = (y-self.y_min)/(self.y_max-self.y_min)
+        return y
+    
+    def norm(self, y):
+        return (y-self.y_min)/(self.y_max-self.y_min)
